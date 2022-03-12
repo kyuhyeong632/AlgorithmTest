f9de7ef02c745e86d301077aa8c3c73ba7bb89ba
kyuhyeong <kkhgud1@naver.om>
2022-03-12 23:10:43 +0900

    시작이 반이다

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(str);
+    }
+}
