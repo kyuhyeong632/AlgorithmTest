d64c53b60638e59a1df65fec48d8673a30e803be
kyuhyeong <kkhgud1@naver.com>
2022-03-14 21:38:44 +0900

    day2

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,18 @@ public class Main {
         System.out.print(str)  ;
     }*/
 
-    /*String to Num Main*/
+    /*String to Num Main
     public static void main(String[] args){
         StringToNum STN = new StringToNum();
         System.out.print(STN.solution("12onezero"));
+    }*/
+
+    /* Finding Kim Seobang in Seoul */
+    public static void main(String[] args){
+        FindKimInSeoul STN = new FindKimInSeoul();
+        String[] seoul = {"Jeong", "Seo", "Ha", "Lee", "Kim", "Yoon"};
+        System.out.print(STN.findKimInSeoul(seoul));
     }
+
 
 }
