8ed886145c37e60217356a7282408b259607b6bb
kyuhyeong <kkhgud1@naver.com>
2022-03-20 22:08:56 +0900

    day8

--- src/FindKimInSeoul.java
+++ src/FindKimInSeoul.java
@@ -5,7 +5,6 @@ public class FindKimInSeoul {
     public String findKimInSeoul(String[] seoul) {
         String answer = "";
         int num = 0;
-
         for(int i = 0; i < seoul.length; i++ ){
             if(seoul[i].equals("Kim")){
                 num = i;
