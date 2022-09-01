81383a1411b98922b23baf22819807232e751d0e
kyuhyeong <kkhgud1@naver.com>
2022-09-01 21:09:21 +0900

    알고리즘

--- src/MultiplyStrings.java
+++ src/MultiplyStrings.java
@@ -3,34 +3,41 @@ import java.util.Arrays;
 public class MultiplyStrings {
 
     public String multiply(String num1, String num2){
-        String str = "";
+        int a = num1.length();
+        int b = num2.length();
 
-        int multi = 0;
-        int digit1 = 1;
-        int digit2 = 1;
+        int[] pos = new int[a+b];
 
-        for(int i = num1.length()-1; i >= 0; i-- ){
-            int topNum = Character.getNumericValue(num1.charAt(i));
-            for(int j = num2.length()-1; j>= 0; j--){
-                int dNum = Character.getNumericValue(num2.charAt(j))*digit2;
-                topNum *= digit1;
-                multi += (topNum * dNum);
-                digit1 *= 10;
+        for(int i = a-1; i >= 0; i-- ){
+            for(int j = b-1; j>= 0; j--){
+                int multi  = (Character.getNumericValue(num1.charAt(i)) * Character.getNumericValue(num2.charAt(j)));
+
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = multi + pos[p2];
+
+                pos[p1] += sum / 10;
+                pos[p2] = (sum) % 10;
 
             }
-            digit1 = 1;
-            digit2 *= 10;
+
         }
 
-        str = Integer.toString(multi);
+        StringBuilder sb = new StringBuilder();
 
-        return str;
+        for(int p : pos){
+            if(!(sb.length() == 0 && p == 0)){
+                sb.append(p);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 
     public static void main(String[] args) {
         MultiplyStrings obj = new MultiplyStrings();
-        String str1 = "123";
-        String str2 = "456";
+        String str1 = "123456789";
+        String str2 = "987654321";
 
         System.out.println("str = " + obj.multiply(str1, str2));
     }
